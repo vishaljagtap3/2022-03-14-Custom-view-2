95ac0fdbd29ede890920bf69d4844fa1f1fdc62f
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-04-12 15:04:16 +0530

    understanding invalidate method

--- app/src/main/java/com/aavidsoft/customview2/TaskView.kt
+++ app/src/main/java/com/aavidsoft/customview2/TaskView.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 
@@ -14,9 +15,14 @@ class TaskView(
 ) : androidx.appcompat.widget.AppCompatTextView(context, attributeSet) {
 
     var taskStatus = false
+    var marginColor = Color.RED
+    private var paint = Paint()
+
 
     init {
         setTextColor(Color.RED)
+        paint.strokeWidth = 20F
+
         setPadding(80, 10, 10, 10)
 
         setOnClickListener {
@@ -31,8 +37,10 @@ class TaskView(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        var paint = Paint()
-        paint.setColor(Color.RED)
+        Log.e("tag", "onDraw()")
+        paint.setColor(marginColor)
+
+
         canvas!!.let {
             it.drawLine(60F, 0F, 60F, height.toFloat(), paint)
             it.drawLine(70F, 0F, 70F, height.toFloat(), paint)
